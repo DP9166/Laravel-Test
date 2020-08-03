53ce096012a0d217a357fa2bda196fab8e749d33
vagrant <vagrant@homestead>
2020-08-03 14:01:11 +0000

    test view single question

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/questions', 'QuestionsController@index');
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/questions/{question}', 'QuestionsController@show');
