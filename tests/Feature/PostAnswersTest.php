e96d054e62b6f1c1e661538752a199868cd4b66c
vagrant <vagrant@homestead>
2020-09-16 09:30:54 +0000

    post answer

--- tests/Feature/PostAnswersTest.php
+++ tests/Feature/PostAnswersTest.php
@@ -13,22 +13,37 @@ class PostAnswersTest extends TestCase
     use RefreshDatabase;
 
     /** @test **/
-    public function user_can_post_an_answer_to_a_question()
+    public function user_can_post_an_answer_to_a_published_question()
     {
-        $question = factory(Question::class)->create();
+        $question = factory(Question::class)->state('published')->create();
         $user = factory(User::class)->create();
 
         $reponse = $this->post("/questions/{$question->id}/answers", [
             'user_id'   =>  $user->id,
             'content'   =>  'This is an answer.'
         ]);
-        /** @test **/
         $reponse->assertStatus(201);
 
         $answer = $question->answers()->where('user_id', $user->id)->first();
         $this->assertNotNull($answer);
 
         $this->assertEquals(1, $question->answers()->count());
+    }
+
+    /** @test **/
+    public function user_not_can_post_an_answer_to_a_unpublished_question()
+    {
+        $question = factory(Question::class)->state('unpublished')->create();
+        $user = factory(User::class)->create();
+
+        $reponse = $this->withExceptionHandling()->post("/questions/{$question->id}/answers", [
+            'user_id'   =>  $user->id,
+            'content'   =>  'This is an answer.'
+        ]);
+        $reponse->assertStatus(404);
+
+        $this->assertDatabaseMissing('answers', ['question_id'  =>  $question->id]);
+        $this->assertEquals(0, $question->answers()->count());
     }
 
     /** @test **/
